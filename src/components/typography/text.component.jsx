1dc3a8364c9cc2643636823ad814f161fbac9385
FungaiPfende <fungaip163@outlook.com>
2022-07-06 20:49:56 +0200

    Created a global text component

---
+++ src/components/typography/text.component.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components/native";
+
+// Create a default style for the text
+const defaultTextStyles = (theme) => `
+  font-family: ${theme.fonts.body};
+  font-weight: ${theme.fontWeights.regular};
+  color: ${theme.colours.text.primary};
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+// Create different style variants
+const body = (theme) => `
+  font-size: ${theme.fontSizes.body};
+`;
+
+const hint = (theme) => `
+  font-size: ${theme.fontSizes.body};
+`;
+
+const error = (theme) => `
+  color: ${theme.colours.text.error}
+`;
+
+const caption = (theme) => `
+  font-size: ${theme.fontSizes.caption};
+  font-weight: ${theme.fontWeights.bold};
+`;
+
+const label = (theme) => `
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes.body};
+  font-weight: ${theme.fontWeights.medium};
+`;
+
+const variants = {
+  body,
+  label,
+  caption,
+  error,
+  hint,
+};
+
+// <Text variant="something" />
+export const Text = styled.Text`
+  ${({ theme }) => defaultTextStyles(theme)};
+  ${({ variant, theme }) => variants[variant](theme)};
+`;
+
+Text.defaultProps = {
+  variant: "body",
+};
