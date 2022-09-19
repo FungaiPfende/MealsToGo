ab5c29ff6bddce03b853dc1635a07d415625828b
FungaiPfende <fungaip163@outlook.com>
2022-09-19 12:42:09 +0200

    Added local data  persistence of `favourites` using `AsyncStorage`.

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (error) {
+      console.log("Error Saving:", error);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      const jsonValue = JSON.parse(value);
+      value != null && setFavourites(jsonValue);
+      // if (value != null) setFavourites(jsonValue);
+    } catch (error) {
+      console.log("Error Loading:", error);
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -16,6 +37,16 @@ export const FavouritesContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  // Load all the `favourites` on the component's first mount
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  // Save the `favourites` to Local Storage anytime the `favourites` change.
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
