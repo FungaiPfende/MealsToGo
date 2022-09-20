fb60d8de8cadf9a35396fc480455dbe6505bb0ab
FungaiPfende <fungaip163@outlook.com>
2022-09-20 14:39:24 +0200

    Initial set up of `AuthNavigator`.

---
+++ src/infrastructure/navigation/authentication.navigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const AccountScreen = () => (
+  <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+    <Text>Account Screen</Text>
+  </View>
+);
+
+const LoginScreen = () => (
+  <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+    <Text>Login Screen</Text>
+  </View>
+);
+
+export const AuthNavigator = () => {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="Main" component={AccountScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+    </Stack.Navigator>
+  );
+};
