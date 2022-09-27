8cfc4fde47c214c78bfbde9931252ed69cb68449
FungaiPfende <fungaip163@outlook.com>
2022-09-27 15:39:05 +0200

    Set up permission request to render out camera.

--- src/features/settings/screens/camera.screen.jsx
+++ src/features/settings/screens/camera.screen.jsx
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import styled from "styled-components";
+import { View } from "react-native";
+
+import { Text } from "../../../components/typography/text.component";
+
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
 
 export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  } else if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>CameraScreen</Text>
-    </View>
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    ></ProfileCamera>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
